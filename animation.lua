a20e746114fe3f123437f5589c7ead1de5fbb934
Liam <glurbozo@gmail.com>
2025-08-27 10:21:03 -0400

    initial commit

---
+++ animation.lua
@@ -0,0 +1,7 @@
+basePath = "Interface\\AddOns\\TwitchEmotes_PPE\\emotes\\";
+TwitchEmotes_animation_metadata[basePath .. "tawow.tga"] = {["nFrames"] = 10, ["frameWidth"] = 96, ["frameHeight"] = 27, ["imageWidth"]= 96, ["imageHeight"]= 270, ["framerate"] = 19}
+TwitchEmotes_animation_metadata[basePath .. "tamhm.tga"] = {["nFrames"] = 9, ["frameWidth"] = 37, ["frameHeight"] = 32, ["imageWidth"]= 37, ["imageHeight"]= 288, ["framerate"] = 4}
+TwitchEmotes_animation_metadata[basePath .. "test.tga"] = {["nFrames"] = 2, ["frameWidth"] = 32, ["frameHeight"] = 32, ["imageWidth"]= 32, ["imageHeight"]= 64, ["framerate"] = 4}
+TwitchEmotes_animation_metadata[basePath .. "peterDance.tga"] = {["nFrames"] = 99, ["frameWidth"] = 32, ["frameHeight"] = 57, ["imageWidth"]= 32, ["imageHeight"]= 5643, ["framerate"] = 12}
+
+
---
+++ emotes.lua
@@ -0,0 +1,8 @@
+basePath = "Interface\\AddOns\\TwitchEmotes_PPE\\emotes\\"
+
+PPE_emotes = {
+	["deadass"] = basePath .. "deadass.tga:28:28",
+	["test"] = basePath .. "test.tga:28:28",
+	["bigStew"] = basePath .. "bigStew.tga:28:28",
+	["peterDance"] = basePath .. "peterDance.tga:28:28"
+}
